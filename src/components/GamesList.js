064366ef7ecb6ef64fb3f695af1fe371345c323d
Aurel <ajfame11@gmail.com>
2021-04-23 12:14:15 -0400

    cleaned gamelist

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import GameCard from './GameCard'
-// import { CardDeck, Card, Form} from 'react-bootstrap'
 import '../index.css'
 
 const GamesList = (props) => {
@@ -8,7 +7,6 @@ const GamesList = (props) => {
 
     return (
         <div style= {{display:'flex', flexDirection:'row', flexWrap:'wrap', justifyContent: 'center'}}>
-            {/* <link rel="stylesheet" href= "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></link> */}
             {games.map(game=> <GameCard key = {game.title} game = {game}/>)}
         </div>
     )
